121b5e9e7d481c3c717481e7cdda6cf6b48ac5fc
natiapa <natiapanosian@gmail.com>
2025-01-18 09:50:53 +0200

    Created Registerpage cmp

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export function RegisterPage() {
+  return (
+    <div className="register-container">
+      <h1>Register</h1>
+      <p>Here we will build the registration form.</p>
+    </div>
+  )
+}
+
+
